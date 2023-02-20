df5f28fdc2cf333ce7be9c91263e4b77828861f5
Sondho <thseogh135@gmail.com>
2023-02-20 22:19:36 +0900

    Add: 초기 환경 세팅

---
+++ src/main/java/sudols/ecopercent/EcopercentApplication.java
@@ -0,0 +1,13 @@
+package sudols.ecopercent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcopercentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcopercentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sudols/ecopercent/EcopercentApplicationTests.java
@@ -0,0 +1,13 @@
+package sudols.ecopercent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcopercentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
