a3f41499af68633921d7bf2ad2d9018e022137b1
Sondho <thseogh135@gmail.com>
2023-02-23 16:12:54 +0900

    fix: @RequestBody() User user 에러 해결
    - Why: @ReqeustBody에 User로 타입으로 데이터를 받을 경우 에러가 발생
    
    - What:
        - 모든 멤버 변수를 받는 생성자를 추가할 경우, 기본 생성자 필요
    
    - How: 모든 멤버 변수를 받는 생성자 삭제

--- src/main/java/sudols/ecopercent/domain/User.java
+++ src/main/java/sudols/ecopercent/domain/User.java
@@ -1,29 +1,16 @@
 package sudols.ecopercent.domain;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 public class User {
     private Long userId;
-    private String nickname;
+    private String nickName;
     private String name;
     private String email;
     private String profileImage;
     private Long titleTumblerId;
     private Long titleEcobagId;
-
-    @Builder
-    public User(Long userId, String nickname, String name, String email, String profileImage, Long titleTumblerId, Long titleEcobagId) {
-        this.userId = userId;
-        this.nickname = nickname;
-        this.name = name;
-        this.email = email;
-        this.profileImage = profileImage;
-        this.titleTumblerId = titleTumblerId;
-        this.titleEcobagId = titleEcobagId;
-    }
 }
