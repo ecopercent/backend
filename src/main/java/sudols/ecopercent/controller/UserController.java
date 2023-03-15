8bfebe3f5bd44bf17ded8dca4fd62e241caf2a50
Sondho <thseogh135@gmail.com>
2023-03-15 17:45:54 +0900

    feat: Usr API 유효성 검사 추가
    - How:
        - UserController.java 에서 ItemService 를 주입 받았는데 제거하고
          UserService.java 에서 ItemRepository 를 주입 받아 사용
        - UserService.java 에서 생각나는 유효성 검사는 전부 구현함.
        - 추가한 유효성 검사는 컨플루언스에서
          '팀별 위키 - 백엔드 - API 유효성 체크 목록' 확인
    
    ECOPERCENT-259

--- src/main/java/sudols/ecopercent/controller/UserController.java
+++ src/main/java/sudols/ecopercent/controller/UserController.java
@@ -13,7 +13,6 @@ import sudols.ecopercent.dto.user.UserTitleItemPatchDto;
 import sudols.ecopercent.service.ItemService;
 import sudols.ecopercent.service.UserService;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,13 +20,10 @@ import java.util.Optional;
 public class UserController {
 
     private final UserService userService;
-    private final ItemService itemService;
 
-    // TODO: 고민. itemService 주입 받아 사용하는게 옳바른 구조인가?
     @Autowired
     public UserController(UserService userService, ItemService itemService) {
         this.userService = userService;
-        this.itemService = itemService;
     }
 
     @PostMapping("/users")
@@ -60,7 +56,6 @@ public class UserController {
         userService.deleteOne(userId);
     }
 
-    // TODO: 고민. UpdateTitleEcobag 과 합칠 수 있는 방법이 없을까?
     @PatchMapping("/users/{userid}/title-tumbler")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.OK)
@@ -84,24 +79,14 @@ public class UserController {
     @ResponseBody
     @ResponseStatus(code = HttpStatus.OK)
     public Optional<Item> GetTitleTumbler(@PathVariable("userid") Long userId) {
-        User user = userService.findOne(userId).get();
-        Long titleTumblerId = user.getTitleTumblerId();
-        System.out.println(titleTumblerId);
-        return itemService.findOne(titleTumblerId);
+        return userService.getTitleTumbler(userId);
     }
 
-    /*
-    TODO: 유효성.
-     - 해당 유저가 존재하는지?
-     - 해당 아이템이 존재하는지?
-    */
     @GetMapping("/users/{userid}/title-ecobag")
     @ResponseBody
     @ResponseStatus(code = HttpStatus.OK)
     public Optional<Item> GetTitleEcobag(@PathVariable("userid") Long userId) {
-        User user = userService.findOne(userId).get();
-        Long titleEcobagId = user.getTitleEcobagId();
-        return itemService.findOne(titleEcobagId);
+        return userService.getTitleEcobag(userId);
     }
 
     // Test
--- src/main/java/sudols/ecopercent/service/UserService.java
+++ src/main/java/sudols/ecopercent/service/UserService.java
@@ -3,10 +3,12 @@ package sudols.ecopercent.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sudols.ecopercent.domain.Item;
 import sudols.ecopercent.domain.User;
 import sudols.ecopercent.dto.user.UserProfilePatchDto;
 import sudols.ecopercent.dto.user.UserProfilePostDto;
 import sudols.ecopercent.dto.user.UserTitleItemPatchDto;
+import sudols.ecopercent.repository.ItemRepository;
 import sudols.ecopercent.repository.UserRepository;
 
 import java.util.List;
@@ -16,22 +18,18 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, ItemRepository itemRepository) {
         this.userRepository = userRepository;
+        this.itemRepository = itemRepository;
     }
 
     public Long join(UserProfilePostDto user) {
+        validateDuplicateUser(user);
         User userEntity = user.toEntity();
         return userRepository.save(userEntity).getUserId();
-    }
-
-    private void validateDuplicateUser(UserProfilePostDto user) {
-        userRepository.findByEmail(user.getEmail())
-                .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 유저입니다.");
-                });
     }
 
     public Optional<User> findOne(Long userId) {
@@ -39,18 +37,59 @@ public class UserService {
     }
 
     public void updateProfile(Long userId, UserProfilePatchDto newUserData) {
+        validateExistUserById(userId);
         userRepository.updateProfile(userId, newUserData.toEntity());
     }
 
     public void updateTitleTumbler(Long userId, UserTitleItemPatchDto newTitleItemData) {
+        validateExistUserById(userId);
+        Optional<Item> optionalItem = itemRepository.findById(newTitleItemData.getTitleTumblerId());
+        if (optionalItem.isPresent() == false) {
+            throw new IllegalStateException("해당 아이템이 존재하지 않습니다.");
+        }
+        if (optionalItem.get().getCategory().equals("tumbler") == false) {
+            throw new IllegalStateException("해당 아이템은 텀블러가 아닙니다.");
+        }
         userRepository.updateTitleTumbler(userId, newTitleItemData.toEntity());
     }
 
     public void updateTitleEcobag(Long userId, UserTitleItemPatchDto newTitleItemData) {
+        validateExistUserById(userId);
+        Optional<Item> optionalItem = itemRepository.findById(newTitleItemData.getTitleEcobagId());
+        if (optionalItem.isPresent() == false) {
+            throw new IllegalStateException("해당 아이템이 존재하지 않습니다.");
+        }
+        System.out.println(optionalItem.get().toString());
+        if (optionalItem.get().getCategory().equals("ecobag") == false) {
+            throw new IllegalStateException("해당 아이템은 에코백이 아닙니다.");
+        }
         userRepository.updateTitleEcobag(userId, newTitleItemData.toEntity());
     }
 
+    public Optional<Item> getTitleTumbler(Long userId) {
+        // TODO: 고민. 아래 두 코드가 중복되는데 어떻게 처리할지?
+        validateExistUserById(userId);
+        User user = userRepository.findById(userId).get();
+        Optional<Item> optionalItem = itemRepository.findById(user.getTitleTumblerId());
+        if (optionalItem.isPresent() == false) {
+            throw new IllegalStateException("해당 아이템이 존재하지 않습니다.");
+        }
+        return optionalItem;
+    }
+
+    public Optional<Item> getTitleEcobag(Long userId) {
+        // TODO: 고민. 아래 두 코드가 중복되는데 어떻게 처리할지?
+        validateExistUserById(userId);
+        User user = userRepository.findById(userId).get();
+        Optional<Item> optionalItem = itemRepository.findById(user.getTitleEcobagId());
+        if (optionalItem.isPresent() == false) {
+            throw new IllegalStateException("해당 아이템이 존재하지 않습니다.");
+        }
+        return optionalItem;
+    }
+
     public void deleteOne(Long userId) {
+        validateExistUserById(userId);
         userRepository.deleteById(userId);
     }
 
@@ -62,5 +101,19 @@ public class UserService {
     // Test
     public void deleteAll() {
         userRepository.clearStore();
+    }
+
+    private void validateDuplicateUser(UserProfilePostDto user) {
+        userRepository.findByEmail(user.getEmail())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 유저입니다.");
+                });
+    }
+
+    private void validateExistUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent() == false) {
+            throw new IllegalStateException("존재하지 않는 유저입니다.");
+        }
     }
 }
