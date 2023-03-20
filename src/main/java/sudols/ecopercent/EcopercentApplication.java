09c56649d7a015d309bfc3748559f8462c383626
Jung-Off <lewis1004@naver.com>
2023-03-20 10:52:20 +0900

    # delete : dbConnection Class 파일 제거
    - How:
        - properties를 수정하면 dbConnection이 필요없다는 것 확인
        - 아예 제거

--- src/main/java/sudols/ecopercent/EcopercentApplication.java
+++ src/main/java/sudols/ecopercent/EcopercentApplication.java
@@ -3,14 +3,10 @@ package sudols.ecopercent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-
 @SpringBootApplication
 public class EcopercentApplication {
 
 	public static void main(String[] args) {
-		dbConnection db = new dbConnection();
-		db.conn("postgres", "jji", "1234");
 		SpringApplication.run(EcopercentApplication.class, args);
 	}
 
