2631c7815823d87fa36e9e25352df493bcf11958
DaehoSon <thseogh135@gmail.com>
2023-06-29 19:45:48 +0900

    refact: repository 두 메서드를 통합
    ECOPERCENT-479

--- src/main/java/sudols/ecopercent/repository/ItemRepository.java
+++ src/main/java/sudols/ecopercent/repository/ItemRepository.java
@@ -1,10 +1,8 @@
 package sudols.ecopercent.repository;
 
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import reactor.util.annotation.NonNull;
-import reactor.util.annotation.NonNullApi;
 import sudols.ecopercent.domain.Item;
 
 import java.util.List;
@@ -13,10 +11,6 @@ import java.util.Optional;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByCategoryAndUser_EmailOrderById(String category, String email);
-
-    Optional<Item> findByIdAndCategory(Long itemId, String category);
-
-    Optional<Item> findByIdAndUser_Email(Long itemId, String email);
 
     @Query("select i from Item i where i.category = ?1 and i.isTitle = true and i.user.email = ?2")
     Optional<Item> findByCategoryAndIsTitleTrueAndUser_Email(String category, String email);
