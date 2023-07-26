b992148e9e02f48b96fb9bb59685f7963bc0a632
DaehoSon <thseogh135@gmail.com>
2023-07-26 15:18:34 +0900

    fix: 유저 닉네임은 특수 문자, 공백, 숫자를 제외한 영어, 한글만 가능
    ECOPERCENT-521

--- src/main/java/sudols/ecopercent/dto/user/CreateUserRequest.java
+++ src/main/java/sudols/ecopercent/dto/user/CreateUserRequest.java
@@ -13,8 +13,7 @@ public class CreateUserRequest {
 
     @NotBlank
     @Size(min = 2, max = 8, message = "User nickname can have a minimum of 2 characters and maximum of 35 characters.")
-    @Pattern(regexp = "^[^\\s]+$", message = "User nickname must not contain spaces.")
-    private String nickname;
+    @Pattern(regexp = "^[a-zA-Z가-힣]+$", message = "User nickname must contain only English and Korean alphabets.")private String nickname;
 
     @Size(max = 35, message = "Profile message can have a maximum of 35 characters.")
     private String profileMessage;
